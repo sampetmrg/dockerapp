e69edc9730d85ab07369481fcc07458fbf4b1d83
sampetmrg <sammy.pettersson@mrggroup.com>
2018-08-24 11:41:47 +0200

    New version

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app.debug = True
 
 @app.route('/')
 def main():
-   return 'Hej!! Min flaskapplikation.'
+   return 'Hej!! Min flaskapplikation i en Docker container.'
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
